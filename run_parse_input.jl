5bd836bbd67d69e0facdc3b86cb0fb2994d12663
Jongsu Liam Kim <jongsukim8@gmail.com>
2019-06-20 20:03:55 +0900

    use data from 2008

--- run_parse_input.jl
+++ run_parse_input.jl
@@ -7,7 +7,7 @@ function read_data()
     obs_path = joinpath(input_dir, "station", "aerosol_observatory_2017.xlsx")
     aes_dir = "aerosol"
     wea_dir = joinpath("weather", "seoul")
-    start_date = ZonedDateTime(2006, 1, 1, 0, 0, tz"Asia/Seoul")
+    start_date = ZonedDateTime(2008, 1, 1, 0, 0, tz"Asia/Seoul")
     end_date = ZonedDateTime(2018, 12, 31, 24, 0, tz"Asia/Seoul")
 
     join_data(input_dir, obs_path, aes_dir, wea_dir, start_date, end_date)
