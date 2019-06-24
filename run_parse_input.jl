ef49a1be04f2e7e31a2e5ae5f070f51dbe1dc928
Jongsu Liam Kim <jongsukim8@gmail.com>
2019-06-25 02:16:51 +0900

    use new observatory data (to remove duplication) and change start date from 2012

--- run_parse_input.jl
+++ run_parse_input.jl
@@ -4,10 +4,10 @@ using MLToys
 
 function read_data()
     input_dir = "/input"
-    obs_path = joinpath(input_dir, "station", "aerosol_observatory_2017.xlsx")
+    obs_path = joinpath(input_dir, "station", "aerosol_observatory_2017_aironly.xlsx")
     aes_dir = "aerosol"
     wea_dir = joinpath("weather", "seoul")
-    start_date = ZonedDateTime(2008, 1, 1, 0, 0, tz"Asia/Seoul")
+    start_date = ZonedDateTime(2012, 1, 1, 0, 0, tz"Asia/Seoul")
     end_date = ZonedDateTime(2018, 12, 31, 24, 0, tz"Asia/Seoul")
 
     join_data(input_dir, obs_path, aes_dir, wea_dir, start_date, end_date)
