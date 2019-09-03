a9ead568b511223138fe547a3237287f722562e1
Jongsu Liam Kim <jongsukim8@gmail.com>
2019-09-04 03:16:40 +0900

    Remove type assertion of model
    to support model which is not constructed by Chain (i.e. LSTM + attention)

--- src/evaluation.jl
+++ src/evaluation.jl
@@ -1,7 +1,7 @@
-evaluations(dataset, model::F, μσ, metrics::Array{String}) where F <: Flux.Chain =
+evaluations(dataset, model, μσ, metrics::Array{String}) =
     evaluations(dataset, model, μσ, Symbol.(metrics))
 
-function evaluations(dataset, model::F, μσ, metrics::Array{Symbol}) where F <: Flux.Chain
+function evaluations(dataset, model, μσ, metrics::Array{Symbol})
     _μ = μσ["total", "μ"][:value]
 
     # initialize array per metric, i.e. RSR_arr
@@ -34,7 +34,7 @@ end
 #=
 RMSE-observations standard deviation ratio 
 =#
-function RMSE(dataset, model::F, μσ::AbstractNDSparse) where F <: Flux.Chain
+function RMSE(dataset, model, μσ::AbstractNDSparse)
     RMSE_arr = Real[]
 
     for (x, y) in dataset
@@ -56,7 +56,7 @@ RMSE(y, ŷ, μ::Real=zero(AbstractFloat)) = sqrt(sum(abs2.(y .- Flux.Tracker.dat
 #=
 RMSE-observations standard deviation ratio 
 =#
-function RSR(dataset, model::F, μσ::AbstractNDSparse) where F <: Flux.Chain
+function RSR(dataset, model, μσ::AbstractNDSparse)
     RSR_arr = Real[]
     μ = μσ["total", "μ"][:value]
 
@@ -78,7 +78,7 @@ normalized statistic that determines the
 relative magnitude of the residual variance (“noise”)
 compared to the measured data variance (“information”
 =#
-function NSE(dataset, model::Flux.Chain, μσ::AbstractNDSparse) where F <: Flux.Chain
+function NSE(dataset, model, μσ::AbstractNDSparse)
     NSE_arr = Real[]
     μ = μσ["total", "μ"][:value]
 
@@ -98,7 +98,7 @@ NSE(y, ŷ, μ::Real=zero(AbstractFloat)) = 1.0 - sqrt(sum(abs2.(y .- Flux.Tracke
 The optimal value of PBIAS is 0.0, with low-magnitude values indicating accurate model simulation. Positive values indicate model underestimation bias, and negative values
 indicate model overestimation bias (Gupta et al., 1999)
 =#
-function PBIAS(dataset, model::F, μσ::AbstractNDSparse) where F <: Flux.Chain
+function PBIAS(dataset, model, μσ::AbstractNDSparse)
     PBIAS_arr = Real[]
 
     for (x, y) in dataset
@@ -113,7 +113,7 @@ end
 
 PBIAS(y, ŷ, μ::Real=zero(AbstractFloat)) = sum(y .- Flux.Tracker.data(ŷ)) / sum(y) * 100.0
 
-function IOA(dataset, model::F, μσ::AbstractNDSparse) where F <: Flux.Chain
+function IOA(dataset, model, μσ::AbstractNDSparse)
     IOA_arr = Real[]
     μ = μσ["total", "μ"][:value]
 
@@ -139,7 +139,7 @@ explains how well your selected independent variable(s)
 explain the variability in your dependent variable(s
 https://en.wikipedia.org/wiki/Coefficient_of_determination
 =#
-function R2(dataset, model::F, μσ::AbstractNDSparse) where F <: Flux.Chain
+function R2(dataset, model, μσ::AbstractNDSparse)
     R2_arr = Real[]
     μ = μσ["total", "μ"][:value]
 
@@ -156,7 +156,7 @@ end
 R2(y, ŷ, μ::Real=zero(AbstractFloat)) = 1.0 - sum(abs2.(y .- Flux.Tracker.data(ŷ))) / sum(abs2.(y .- μ))
 
 
-function RAE(Edataset, model::F, μσ::AbstractNDSparse) where F <: Flux.Chain
+function RAE(Edataset, model, μσ::AbstractNDSparse)
     RAE_arr = Real[]
     μ = μσ["total", "μ"][:value]
 
@@ -192,7 +192,7 @@ end
 
 AdjR2(y, ŷ, μ::Real=zero(AbstractFloat)) = 1.0 - sum(abs2.(y .- Flux.Tracker.data(ŷ))) / sum(abs2.(y .- μ))
 =#
-function classification(dataset::Array{T1, 1}, model::C, ycol::Symbol) where C <: Flux.Chain where T1 <: Tuple{AbstractArray{F, 1}, AbstractArray{F, 1}} where F <: AbstractFloat
+function classification(dataset::Array{T1, 1}, model, ycol::Symbol) where T1 <: Tuple{AbstractArray{F, 1}, AbstractArray{F, 1}} where F <: AbstractFloat
     class_all_arr = []
     class_high_arr = []
 
